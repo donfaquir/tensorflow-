e9896ad0b24d573c76635697bd621361456152bc
caicq <caicq225@163.com>
2018-03-19 23:21:41 +0800

    add 123.py as exercesing for mnist in lijiaxuan\'book

---
+++ 123.py
@@ -0,0 +1,96 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+INPUT_NODE = 784
+OUTPUT_NODE = 10
+
+LAYER1_NODE = 500
+
+BATCH_SIZE = 100
+
+LEARNING_RATE_BASE = 0.8
+LEARNING_RATE_DECAY = 0.99
+
+REGULARIZATION_RATE = 0.0001
+TRAINING_STEPS = 3000
+MOVING_AVERAGE_DECAY = 0.99
+
+def inference(input_tensor,avg_class,weight1,biases1,weight2,biases2):
+	if avg_class == None:
+		layer1 = tf.nn.relu(tf.matmul(input_tensor,weight1)+biases1)
+		return tf.matmul(layer1,weight2) + biases2
+	else:
+		layer1 = tf.nn.relu(tf.matmul(input_tensor,avg_class.average(weight1)) + 
+			avg_class.average(biases1))
+		return tf.matmul(layer1,avg_class.average(weight2)) + avg_class.average(biases2)
+def train(mnist):
+	x = tf.placeholder(tf.float32,[None,INPUT_NODE],name = 'x-input')
+	y_ = tf.placeholder(tf.float32,[None,OUTPUT_NODE],name='y-input')
+	
+	weight1 = tf.Variable(tf.truncated_normal([INPUT_NODE,LAYER1_NODE], stddev = 0.1))
+	biases1 = tf.Variable(tf.constant(0.1,shap = [LAYER1_NODE]))
+	
+	weight2 = tf.Variable( tf.truncated_normal( [LAYER1_NODE,OUTPUT_NODE],stddev=0.1))
+	biases2 = tf.Variable( tf.constant(0.1,shape=[OUTPUT_NODE]))
+	
+	y = inference(x,None,weight1,biases1,weight2,biases2)
+	
+	global_step = tf.Variable(0, trainable=False)
+	
+	variable_average = tf.train.ExponentialMovingAverage( MOVING_AVERAGE_DECAY,global_step)
+	
+	variable_average_op = variable_average.apply( tf.trainable_variables())
+	
+	average_y = inference(x,variable_average,weight1,biases1,weight2,biases2)
+	
+	cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(y,tf.argmax(y_,1))
+	cross_entropy_mean = tf.reduce_mean(cross_entropy)
+	
+	regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
+	regularization = regularizer(weight1) + regularizer(weight2)
+	
+	loss = cross_entropy_mean + regularization
+	
+	learing_rete = tf.train.exponential_decay( LEARNING_RATE_BASE, global_step,
+		mnist.train.num_examples/BATCH_SIZE, LEARNING_RATE_DECAY )
+	
+train_step = tf.train.GradientDescentOptimizer(learing_rete).minimize(loss,global_step=global_step)
+
+with tf.control_dependencies([train_step,variable_average_op]):
+	train_op = tf.no_op(name='train')
+
+correct_prediction = tf.equal(tf.argmax(average_y,1),tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.case(correct_prediction,tf.float32))
+
+with tf.Session() as sess:
+	tf.initialize_all_variables().run()
+	
+	validate_feed = {x:mnist.validation.images,y:mnist,validation.labels}
+	
+	test_feed = {x:mnist.test.images,y:mnist.test.labels}
+	
+	for i in range(TRAINING_STEPS):
+		if i%100 == 0:
+			validate_acc = sess.run(accuracy,feed_dict = validate_feed)
+			print("After %d trainint step(s),validation accuracy"
+				"using average model is %g" % (i,validate_acc))
+		xs,ys = mnist.train.next_batch(BATCH_SIZE)
+		sess.run(train_op,feed_dict=(x:xs,y_:ys))
+	test_acc = sess.run(accuracy,feed_dict=test_feed)
+	print("After %d trainint step(s),validation accuracy"
+		"using average model is %g" % (TRAINING_STEPS,test_acc))
+
+def main(argv=None):
+	mnist = input_data.read_data_sets('./MNIST_data', one_hot=True)
+	train(mnist)
+	
+if __name__ == '__main__':
+	tf.app.run()
+	
+	
+		
+		
+		
+		
+	
--- test4ema.py
+++ test4ema.py
@@ -7,24 +7,24 @@ v1 = tf.Variable(0,dtype=tf.float32)
 
 step = tf.Variable(0,trainable=False) 
 
-ema = tf.train.ExponentiaMovingAverage(0.99,step)
+ema = tf.train.ExponentialMovingAverage(0.99,step)
 
 maintain_average_op = ema.apply([v1])
 
 with tf.Session() as sess:
-	init_op = tf.initialize_all_variables()
+	init_op = tf.global_variables_initializer()
 	sess.run(init_op)
 	
-	print sess.run([v1,ema.average(v1)])
+	print(sess.run([v1,ema.average(v1)]))
 	
 	sess.run(tf.assign(v1,5))
 	sess.run(maintain_average_op)
-	print sess.run([v1,ema.average(v1)])
+	print(sess.run([v1,ema.average(v1)]))
 	
 	sess.run(tf.assign(step,1000))
 	sess.run(tf.assign(v1,10))
 	sess.run(maintain_average_op)
-	print sess.run([v1,ema.average(v1)])
+	print(sess.run([v1,ema.average(v1)]))
 	
 	sess.run(maintain_average_op)
-	print sess.run([v1,ema.average(v1)])
+	print(sess.run([v1,ema.average(v1)]))
